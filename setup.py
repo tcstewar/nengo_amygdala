cc7565680210843706784707ed88b947ce18c312
Terry Stewart <tcstewar@uwaterloo.ca>
2018-07-11 17:49:22 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='nengo_amygdala',
+    packages=['nengo_amygdala'],
+    author='Kate Fishl and Terry Stewart',
+    url='https://github.com/tcstewar/nengo_amygdala',
+    license='GPLv3',
+    )
